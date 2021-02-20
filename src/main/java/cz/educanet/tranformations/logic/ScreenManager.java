ce0e0485dc0b21fd7bd14e63a11945646f2ea0ba
KosacekCZ <35407839+KosacekCZ@users.noreply.github.com>
2021-02-20 15:33:02 +0100

    Initial commit

---
+++ src/main/java/cz/educanet/tranformations/logic/ScreenManager.java
@@ -0,0 +1,131 @@
+package cz.educanet.tranformations.logic;
+
+import cz.educanet.tranformations.logic.models.Coordinate;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ScreenManager {
+
+    int pos = 0;
+
+    private Set<Coordinate> selectedPoints = new HashSet<>();
+
+    public void select(Coordinate coordinate) {
+        selectedPoints.add(coordinate);
+    }
+
+    public void unselect(Coordinate coordinate) {
+        selectedPoints.remove(coordinate);
+    }
+
+    public boolean isSelected(Coordinate coordinate) {
+        return selectedPoints.contains(coordinate);
+    }
+
+    public Set<Coordinate> getSelectedPoints() {
+        return selectedPoints;
+    }
+
+    public boolean isFilledIn(Coordinate coordinate) {
+        if(getSelectedPoints().size() < 3) {
+            return false;
+        }
+        Coordinate[] xyArray = new Coordinate[3];
+        int i = 0;
+        for (Coordinate getCoords : getSelectedPoints()) {
+            xyArray[i] = getCoords;
+            i++;
+        }
+
+
+        float a1 = (float)(xyArray[1].getY() - xyArray[0].getY()) / (xyArray[1].getX() - xyArray[0].getX());
+        float b1 = xyArray[0].getY() - a1 * xyArray[0].getX();
+
+        float a2= (float)(xyArray[2].getY() - xyArray[1].getY()) / (xyArray[2].getX() - xyArray[1].getX());
+        float b2 = xyArray[1].getY() - a2 * xyArray[1].getX();
+
+        float a3 = (float)(xyArray[0].getY() - xyArray[2].getY()) / (xyArray[0].getX() - xyArray[2].getX());
+        float b3 = xyArray[2].getY() - a3 * xyArray[2].getX();
+
+        boolean one = false;
+        boolean two = false;
+        boolean three = false;
+
+        if (xyArray[2].getY() > a1 * xyArray[2].getX() + b1) {
+            one = true;
+        } else if (xyArray[2].getY() < a1 * xyArray[2].getX() + b1) {
+            one = false;
+        }
+
+        if (xyArray[0].getY() > a2 * xyArray[0].getX() + b2) {
+            two = true;
+        } else if (xyArray[0].getY() < a2 * xyArray[0].getX() + b2) {
+            two = false;
+        }
+
+        if (xyArray[1].getY() > a3 * xyArray[1].getX() + b3) {
+            three = true;
+        } else if (xyArray[1].getY() < a3 * xyArray[1].getX() + b3) {
+            three = false;
+        }
+
+        if (one && two && three) {
+            if (coordinate.getY() >= a1 * coordinate.getX() + b1 &&
+                    coordinate.getY() >= a2 * coordinate.getX() + b2 &&
+                    coordinate.getY() >= a3 * coordinate.getX() + b3) {
+                return true;
+            }
+        } else if (one && two && !three) {
+            if (coordinate.getY() >= a1 * coordinate.getX() + b1 &&
+                    coordinate.getY() >= a2 * coordinate.getX() + b2 &&
+                    coordinate.getY() <= a3 * coordinate.getX() + b3) {
+                return true;
+            }
+
+        } else if (one && !two && three) {
+            if (coordinate.getY() >= a1 * coordinate.getX() + b1 &&
+                    coordinate.getY() <= a2 * coordinate.getX() + b2 &&
+                    coordinate.getY() >= a3 * coordinate.getX() + b3) {
+                return true;
+            }
+
+        } else if (!one && two && three) {
+            if (coordinate.getY() <= a1 * coordinate.getX() + b1 &&
+                    coordinate.getY() >= a2 * coordinate.getX() + b2 &&
+                    coordinate.getY() >= a3 * coordinate.getX() + b3) {
+                return true;
+            }
+
+        } else if (!one && !two && three) {
+            if (coordinate.getY() <= a1 * coordinate.getX() + b1 &&
+                    coordinate.getY() <= a2 * coordinate.getX() + b2 &&
+                    coordinate.getY() >= a3 * coordinate.getX() + b3) {
+                return true;
+            }
+
+        } else if (!one && two && !three) {
+            if (coordinate.getY() <= a1 * coordinate.getX() + b1 &&
+                    coordinate.getY() >= a2 * coordinate.getX() + b2 &&
+                    coordinate.getY() <= a3 * coordinate.getX() + b3) {
+                return true;
+            }
+
+        } else if (one && !two && !three) {
+            if (coordinate.getY() >= a1 * coordinate.getX() + b1 &&
+                    coordinate.getY() <= a2 * coordinate.getX() + b2 &&
+                    coordinate.getY() <= a3 * coordinate.getX() + b3) {
+                return true;
+            }
+
+        } else if (!one && !two && !three) {
+            if (coordinate.getY() <= a1 * coordinate.getX() + b1 &&
+                    coordinate.getY() <= a2 * coordinate.getX() + b2 &&
+                    coordinate.getY() <= a3 * coordinate.getX() + b3) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+}
---
+++ src/main/java/cz/educanet/tranformations/presentation/canvas/CanvasMouseListener.java
@@ -0,0 +1,39 @@
+package cz.educanet.tranformations.presentation.canvas;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class CanvasMouseListener implements MouseListener {
+
+    private MouseClickListener listener = null;
+
+    public CanvasMouseListener(MouseClickListener mouseClickListener) {
+        this.listener = mouseClickListener;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent mouseEvent) {
+        listener.click(mouseEvent.getX(), mouseEvent.getY(), mouseEvent.getButton());
+        System.out.println(mouseEvent.getX() + " " + mouseEvent.getY());
+    }
+
+    @Override
+    public void mousePressed(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent mouseEvent) {
+
+    }
+}
